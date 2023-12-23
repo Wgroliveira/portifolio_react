161b4ca47487d27ee76d1f8583db8708a822ac3b
Wgroliveira <129519069+Wgroliveira@users.noreply.github.com>
2023-12-23 17:31:42 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema,SidebarContainer } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Wgroliveira.png" />
-    <Titulo fontSize={20}>Wagner Antônio</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Wagner Antônio</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Wgroliveira
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
